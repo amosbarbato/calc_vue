6605872364e7d26639e01695064de7d8a15cf107
Amos Barbato <amosbarbato@gmail.com>
2023-07-12 12:12:43 -0300

    final source

--- src/components/page.vue
+++ src/components/page.vue
@@ -1,30 +1,58 @@
 <script>
-
+    export default {
+        data() {
+            return {
+            result: null,
+            operand1: 0,
+            operand2: 0
+            };
+        },
+        methods: {
+            performOperation(operation) {
+                switch (operation) {
+                    case '+':
+                    this.result = this.operand1 + this.operand2;
+                    break;
+                    case '-':
+                    this.result = this.operand1 - this.operand2;
+                    break;
+                    case '*':
+                    this.result = this.operand1 * this.operand2;
+                    break;
+                    case '/':
+                    this.result = this.operand1 / this.operand2;
+                    break;
+                }
+            }
+        }
+    };
 </script>
 
-
 <template>
-    <div class="container">
-        
-        <div class="row">
-            <div class="col-md-6">##</div>
-            <div class="col-md-6">
+    <div class="container px-5 py-5 shadow">       
+        <div class="row align-items-center">
+            <div class="col-10 col-sm-8 col-lg-6">
+                <div class="hero content">
+                    <img src="../assets/img/operations-colored-icons-by-Vexels.png" class="d-block pe-4 img-fluid" alt="Bootstrap Themes" width="600" height="500" loading="lazy">
+                </div>                
+            </div>
+            <div class="col-lg-6">
                 <div class="calculator">
                     <div class="content">
                         <div class="calc-num">
                             <label class="calc-header">Digite um número</label>
-                            <input v-model="num1" type="number" placeholder="Digite aqui">
+                            <input v-model="operand1" type="number" placeholder="Digite aqui">
                         </div>
-                        <div class="calc-operator">
-                            <button value="+" class="oper-toggle" @click="toggle">+</button>
-                            <button value="-" class="oper-toggle" @click="toggle">-</button>
-                            <button value="*" class="oper-toggle" @click="toggle">x</button>
-                            <button value="/" class="oper-toggle" @click="toggle">/</button>
-                        </div> 
                         <div class="calc-num">
                             <label class="calc-header">Digite um número</label>
-                            <input v-model="num1" type="number" placeholder="Digite aqui">
+                            <input v-model="operand2" type="number" placeholder="Digite aqui">
                         </div>
+                        <div class="calc-operator">
+                            <button @click="performOperation('+')">+</button>
+                            <button @click="performOperation('-')">-</button>
+                            <button @click="performOperation('*')">x</button>
+                            <button @click="performOperation('/')">/</button>
+                        </div>                      
                         <div class="calc-result">
                             <h6 class="calc-header">
                                 Resultado: {{ result }}
@@ -39,15 +67,18 @@
 
 <style scoped>
     .container {
-        margin: 100px auto;
         background-color: white;
         border-radius: 20px;
+        position:absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
     }
     .calculator {
         border-radius: 15px;
-        margin: 30px 18px 30px 0;
         padding: 40px;
-        background-color: #004749;
+        background-color: var(--dark-primary);
+        height: 450px;
     }
     .calc-num {
         display: flex;
@@ -64,10 +95,10 @@
         gap: 15px;
         margin-bottom: 25px;
     }
-
     .calc-result{
-        background-color: #26C1AF;
-        color: #013e41;
+        margin-top: 138px;
+        background-color: var(--secondary);
+        color: var(--primary);
         font-weight: 700;
         text-align: center;
         border: none;
@@ -75,7 +106,6 @@
         padding: 15px;
         outline: none;
     }
-
     .calc-result h6 {
         margin: 0;
         text-transform: uppercase;
